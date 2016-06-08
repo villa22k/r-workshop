c7ea36827c970051006643e286ba30c2f223dd47
Eddie Pantridge <EPantridge@MassMutual.com>
2016-06-08 09:45:31 -0400

    Adding all project files

--- Control_Flow_Practice.R
+++ Control_Flow_Practice.R
@@ -50,14 +50,7 @@ microbenchmark({
 }) # Median = 976.0524 millisecs
 
 # Method 1
-o2 <- rep(NA, 1e6)
-microbenchmark({
-  for (i in 1:length(x)) {
-    ifelse(x[i] < 0,
-           o2[i] <- 'Negative',
-           o2[i] <- 'Positive')
-  }
-}) # Median = 
+# Median = 
 
 # Methad 2
 o3 <- rep(NA, 1e6)
@@ -65,3 +58,78 @@ microbenchmark({
   o3[x < 0] <- 'Negative'
   o3[x >= 0] <- 'Positive'
 }) # Median  = 17.63954 millisecs
+
+
+#################
+# Set 3
+
+x <- 0
+while(x < 7) {
+  x <- x + 1
+  if (x %% 2 == 0) next
+  print(x)
+}
+
+
+################
+# Set 4
+
+set.seed(157)
+m <- matrix(c(rnorm(30, 0), rnorm(30, 2), rnorm(30, 5),
+              nrow = 30, ncol = 3))
+  
+# Method 1
+microbenchmark({
+  apply(m, 2, mean)
+}) # mean = 20.50995
+
+# Method 2
+microbenchmark({
+  colMeans(m)
+}) # mean = 3.95014
+
+# Method 3
+microbenchmark({
+  o <- rep(NA, 3)
+  for (i in 1:3) {
+    o[i] <- mean(m[i,])
+  }
+}) # mean = 15.36912
+
+# Method 4
+microbenchmark({
+  o2 <- rep(NA, 3)
+  for (i in 1:dim(m)[1]) {
+    r <- rep(NA, dim(m)[2])
+    for (j in 1:dim(m)[2]) {
+      r[j] <- m[i,j]
+    }
+  o2[i] <- mean(r)
+  }
+}) # mean = 561.3342
+
+# Method 5
+microbenchmark({
+  o2 <- rep(NA, 3)
+  for (i in 1:nrow(m)) {
+    r <- rep(NA, ncol(m))
+    for (j in 1:ncol(m)) {
+      r[j] <- m[i,j]
+    }
+    o2[i] <- mean(r)
+  }
+}) # mean = 610.2942
+
+
+
+######################
+# Functions
+
+g1 <- function(x) {
+  return(x + 1)
+}
+g1(5)
+
+
+g2 <- function(x, y)  (x ** 2) + (y ** 2)
+g2(3, 4)
